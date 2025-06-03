f47eb47df3583d5fe8714981fc865830dd1bf430
王浩 <2386800113@qq.com>
2025-06-03 17:14:25 +0800

    新增缩放方法，应对不同电脑的缩放和分辨率设置

--- src/main/java/hao/mousedefibrillator/tools/ClickTool.java
+++ src/main/java/hao/mousedefibrillator/tools/ClickTool.java
@@ -1,6 +1,11 @@
 package hao.mousedefibrillator.tools;
 
+import com.sun.jna.platform.win32.GDI32;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.platform.win32.WinUser;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.util.concurrent.*;
 
 import static hao.mousedefibrillator.config.GenerateIni.CLICK_DELAY;
@@ -47,17 +52,45 @@ public class ClickTool {
     }
 
     /**
+     * 获取Windows当前缩放比例 (100% = 1.0, 125% = 1.25, 150% = 1.5)
+     */
+    private Point getScalingFactor(int x, int y) {
+        User32 user32 = User32.INSTANCE;
+        GDI32 gdi32 = GDI32.INSTANCE;
+        WinDef.HWND hwnd = user32.GetDesktopWindow();
+        WinDef.HDC hdc = user32.GetDC(hwnd);
+        int dpi = gdi32.GetDeviceCaps(hdc, 88); // 使用 GDI32 调用
+        user32.ReleaseDC(hwnd, hdc);
+        double scaling = dpi / 96.0; // 计算缩放比例
+        System.out.println(scaling);
+        return new Point((int)(x / scaling), (int)(y / scaling));
+    }
+
+    /**
      * 根据坐标：执行单击或双击
      * @param button  鼠标按钮：InputEvent.BUTTON1_DOWN_MASK（左键）或 InputEvent.BUTTON3_DOWN_MASK（右键）
      * @param singleClick 是否单击
      */
     private void cursorPerformClick(int x, int y, int button, boolean singleClick){
         try {
-            robot.mouseMove(x, y);
+            robot.mouseMove(-1, -1);
+
+            // 获取缩放因子
+            GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+            GraphicsConfiguration gc = gd.getDefaultConfiguration();
+            AffineTransform at = gc.getDefaultTransform();
+            double scaleX = at.getScaleX();
+            double scaleY = at.getScaleY();
+            // 检查是否需要转换（逻辑像素）
+            if (scaleX > 1.01 || scaleY > 1.01) {
+                robot.mouseMove(x, y);
+            }else {
+                Point point = getScalingFactor(x, y);
+                robot.mouseMove(point.x, point.y);
+            }
             robot.mousePress(button);
             Thread.sleep(CLICK_DELAY);
             robot.mouseRelease(button);
-
 
             if (!singleClick) { // 如果是双击
                 robot.mousePress(button);
